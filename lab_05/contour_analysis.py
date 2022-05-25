5dbdbab38cb0ed55c900fdefb2ebe7509b616f63
kosarevskiydp <kosarevskiydp@pik.ru>
2022-05-26 01:05:38 +0300

    upd lab_05: not so good...

--- lab_05/contour_analysis.py
+++ lab_05/contour_analysis.py
@@ -4,10 +4,9 @@ import numpy as np
 
 from utils.utils import loader
 
-# URL = "https://images.pexels.com/photos/11572738/pexels-photo-11572738.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
-# PATTERN_URL = "https://i.ibb.co/c1Jp7Rd/Screenshot-2022-05-23-at-21-32-30.png"
-URL = "https://i.ibb.co/RjLN0rL/mario.png"
-PATTERN_URL = "https://i.ibb.co/GCWLkGV/mario-coin.jpg"
+PATTERN_URL = "https://img.favpng.com/18/12/25/triangle-blue-shape-png-favpng-GiyVGMhuhVfrT688Zw5x4DGXH.jpg"
+URL = "https://mpng.subpng.com/20190607/clo/kisspng-triangle-symmetry-pattern-tessellation-filetriangular-orthobianticupola-net-png-wikipe-5cfa077caa4479.7233607715598897886974.jpg"
+COLOR = (0, 255, 0)
 
 
 def description():
@@ -65,97 +64,68 @@ def get_contours(gray, color):
     c2.image(image_copy)
 
 
-def match_by_template(c_img, img, pattern):
+def match_by_template(c_img, img, pattern, threshold=0.8):
     w, h = pattern.shape[::-1]
 
     res = cv2.matchTemplate(img, pattern, cv2.TM_CCOEFF_NORMED)
-    threshold = 0.8
+
     loc = np.where(res >= threshold)
 
     for pt in zip(*loc[::-1]):
-        cv2.rectangle(c_img, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
+        cv2.rectangle(c_img, pt, (pt[0] + w, pt[1] + h), COLOR, 2)
 
     st.image(c_img)
 
 
-def equlizehist(image_path, denoise=False, verbose=False):
-    bgr = cv2.imread(image_path)
-    bgr[:, :, 0] = cv2.equalizeHist(bgr[:, :, 0])
-    bgr[:, :, 1] = cv2.equalizeHist(bgr[:, :, 1])
-    bgr[:, :, 2] = cv2.equalizeHist(bgr[:, :, 2])
-
-    if denoise:
-        # bgr = cv2.fastNlMeansDenoisingColoredMulti([bgr, bgr, bgr, bgr, bgr], 2, 5, None, 4, 5, 35)
-        bgr = cv2.fastNlMeansDenoisingColored(bgr, None, 10, 10, 7, 21)
-
-    if verbose:
-        cv2.imshow("test", bgr)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
-    return bgr
-
-
-def histogram_equalization(filepath):
-    intensitiesList = []
-    frequencyList = []
-    cumulativeFrequencyList = []
-
-    img = cv2.imread(filepath, cv2.CV_LOAD_IMAGE_GRAYSCALE)
-    numberOfPixels = img.shape[0] * img.shape[1]
-
-    for row in range(0, img.shape[0]):
-        for column in range(0, img.shape[1]):
-            if img[row, column] not in intensitiesList:
-                intensitiesList.extend([img[row, column]])
-
-    # print 'Intensities in the image: ', str(intensitiesList)
-
-    intensitiesList.sort()
-    # print 'Sorted intensities in the image: ', str(intensitiesList)
-
-    for intensity in intensitiesList:
-        count = 0
-        for row in range(0, img.shape[0]):
-            for column in range(0, img.shape[1]):
-                if intensity == img[row, column]:
-                    count += 1
-
-        frequencyList.extend([count])
-        if not cumulativeFrequencyList:
-            cumulativeFrequencyList.extend([count])
-        else:
-            cumulativeValue = cumulativeFrequencyList[len(cumulativeFrequencyList) - 1] + count
-            cumulativeFrequencyList.extend([cumulativeValue])
-
-    # print 'Frequencies of each intensity (Sorted intensities): ', str(frequencyList)
-
-    # print 'Cumulative frequencies: ', str(cumulativeFrequencyList)
-
-    for row in range(0, img.shape[0]):
-        for column in range(0, img.shape[1]):
-            i = intensitiesList.index(img[row, column])
-            img[row, column] = int(((cumulativeFrequencyList[i] - cumulativeFrequencyList[0]) / (
-                        numberOfPixels - cumulativeFrequencyList[0])) * (255))
-
-    cv2.imwrite('..\\results\\histogram_equalization.png', img)
-
-
 def main():
-    # TODO drop:
-    #         https://robocraft.ru/computervision/640
-    #         https://docs.opencv.org/3.3.0/d4/dc6/tutorial_py_template_matching.html
-    #         https://www.geeksforgeeks.org/how-to-detect-shapes-in-images-in-python-using-opencv/
 
     description()
 
+    c_pattern, gray_pattern = loader(PATTERN_URL, txt="паттерн")
+    # get_contours(gray_pattern, c_pattern)
+
     color_img, gray_image = loader(URL, txt="изображение")
+    h, w = gray_image.shape[:2]
     # get_contours(gray_image, color_img)
     st.markdown("---")
 
-    c_pattern, gray_pattern = loader(PATTERN_URL, txt="паттерн")
-    get_contours(gray_pattern, c_pattern)
+    # Template matching using edges instead of the original image can greatly improve the accuracy of template matching.
+    found = []
+    (tH, tW) = gray_pattern.shape[:2]
+    template_edged = cv2.Canny(gray_pattern, 50, 200)
 
-    match_by_template(color_img, gray_image, c_pattern)
+    st.image(template_edged)
+
+    # Traverse the image size
+    i = 0
+    for scale in np.linspace(1, 2, 20):
+        resized = cv2.resize(gray_image, dsize=(0, 0), fx=scale, fy=scale)
+
+        r = gray_image.shape[1] / float(resized.shape[1])
+
+        if resized.shape[0] < tH or resized.shape[1] < tW:
+            break
+        edged = cv2.Canny(resized, 50, 200)
+        result = cv2.matchTemplate(edged, template_edged, cv2.TM_CCOEFF)
+        (_, maxVal, _, maxLoc) = cv2.minMaxLoc(result)
+
+        if not found:
+            found.append((maxVal, maxLoc, r))
+
+        if maxVal > found[i][0]:
+            found.append((maxVal, maxLoc, r))
+            i += 1
+
+    for f in found:
+        (_, maxLoc, r) = f
+        (startX, startY) = (int(maxLoc[0] * r), int(maxLoc[1] * r))
+        (endX, endY) = (int((maxLoc[0] + tW) * r), int((maxLoc[1] + tH) * r))
+        color_img = np.asarray(color_img)
+        cv2.rectangle(color_img, (startX, startY), (endX, endY), COLOR, 2)
+
+    st.image(color_img)
+
+    # match_by_template(color_img, gray_image, gray_pattern)
     # match_by_template(gray_pattern, gray_pattern)
 
 
