bd36a43ddc0d618417c977dbee3d88af14ff9e90
kosarevskiydp <kosarevskiydp@pik.ru>
2022-05-28 20:57:24 +0300

    fix lab_06: add custom non_max_suppression

--- lab_06/key_points.py
+++ lab_06/key_points.py
@@ -39,40 +39,52 @@ def description():
     st.markdown("---")
 
 
-def moravec_detector(gray, color, block_size, thresh, non_max_supp):
+def moravec_detector(img_gray, img, block_size, threshold):
     r = block_size // 2
-    rows = gray.shape[0]
-    cols = gray.shape[1]
-    corners = []
+    row = img_gray.shape[0]
+    col = img_gray.shape[1]
 
-    for y in range(r, rows - r):
-        for x in range(r, cols - r):
+    min_val = np.zeros(img_gray.shape)
+    count = 0
+
+    for y in range(r, row - r):
+        for x in range(r, col - r):
             v1 = 0
             v2 = 0
             v3 = 0
             v4 = 0
             for k in range(-r, r):
-                v1 += np.square(gray[y, x + k] - gray[y, x + k + 1])
-                v2 += np.square(gray[y + k, x] - gray[y + k + 1, x])
-                v3 += np.square(gray[y + k, x + k] - gray[y + k + 1, x + k + 1])
-                v4 += np.square(gray[y + k, x - k] - gray[y + k + 1, x - k - 1])
-            arr = np.array([v1, v2, v3, v4])
-            if min(arr) > thresh:
-                corners.append((x, y))
+                v1 += np.square(img_gray[y, x + k] - img_gray[y, x + k + 1])
+                v2 += np.square(img_gray[y + k, x] - img_gray[y + k + 1, x])
+                v3 += np.square(img_gray[y + k, x + k] - img_gray[y + k + 1, x + k + 1])
+                v4 += np.square(img_gray[y + k, x - k] - img_gray[y + k + 1, x - k - 1])
 
-    for corner in corners:
-        cv2.circle(color, corner, 3, COLOR)
+            min_val[y, x] = min(v1, v2, v3, v4)
+
+            if min_val[y, x] < threshold:
+                min_val[y, x] = 0
+            else:
+                count += 1
+
+    for i in range(block_size, row - block_size):
+        for j in range(block_size, col - block_size):
+            mat = min_val[i - block_size:i + block_size, j - block_size:j + block_size]
+            if np.max(mat) != 0:
+                pos = np.unravel_index(np.argmax(mat), mat.shape)
+                corn_y = i + pos[0] - block_size
+                corn_x = j + pos[1] - block_size
+                cv2.circle(img, (corn_x, corn_y), 3, COLOR)
 
     st.write("Детектор Моравеца.")
-    st.write(f"Кол-во углов: {len(corners)}")
+    st.write(f"Кол-во углов: {count}")
 
-    return color
+    return img
 
 
-def harris_detector(gray, color, block_size, aperture_size, k):
-    gray = np.float32(gray)
+def harris_detector(img_gray, img, block_size, aperture_size, k):
+    img_gray = np.float32(img_gray)
 
-    dest = cv2.cornerHarris(gray, block_size, aperture_size, k)
+    dest = cv2.cornerHarris(img_gray, block_size, aperture_size, k)
     thresh = 0.01 * dest.max()
     num_corners = np.sum(dest > thresh)
     dest = cv2.dilate(dest, None)
@@ -80,19 +92,19 @@ def harris_detector(gray, color, block_size, aperture_size, k):
     for i in range(dest.shape[0]):
         for j in range(dest.shape[1]):
             if int(dest[i, j]) > thresh:
-                cv2.circle(color, (j, i), 3, COLOR)
+                cv2.circle(img, (j, i), 3, COLOR)
 
     st.write("Детектор Харриса-Стефана.")
     st.write(f"Кол-во углов: {num_corners}")
 
-    return color
+    return img
 
 
-def fast_detector(gray, color, thresh, non_max_suppression):
+def fast_detector(img_gray, img, thresh, non_max_suppression):
     fast = cv2.FastFeatureDetector_create(thresh, non_max_suppression)
 
-    corners = fast.detect(gray, None)
-    img = cv2.drawKeypoints(color, corners, None, color=COLOR)
+    corners = fast.detect(img_gray, None)
+    img = cv2.drawKeypoints(img, corners, None, color=COLOR)
 
     st.write("Детектор FAST.")
     st.write(f"Кол-во углов: {len(corners)}")
@@ -118,12 +130,10 @@ def main():
     st.markdown("---")
 
     if detector == "1":
-        c3, c4, cs = st.columns(3)
+        c3, c4 = st.columns(2)
         block_size = c3.number_input("Размер блока:", min_value=1, max_value=99, value=5, step=1)
         thresh = c4.number_input("Порог:", min_value=1, max_value=9999, value=300, step=1)
-        non_max_supp = cs.checkbox("Подавление не-максимумов", value=True)
-        moravec_img = moravec_detector(gray_image.copy(), color_img.copy(), block_size, thresh, non_max_supp)
-        # moravec_img = moravec_detector_2(gray_image.copy(), color_img.copy(), block_size, thresh, non_max_supp)
+        moravec_img = moravec_detector(gray_image.copy(), color_img.copy(), block_size, thresh)
         st.image(moravec_img)
         st.markdown("---")
 
